6531e668031d939e2d722ae1b0cb71413a43746b
Spacegod007 <spacegod007@outlook.com>
2018-04-24 16:48:14 +0200

    vraag 5

--- src/test/java/util/DatabaseTest.java
+++ src/test/java/util/DatabaseTest.java
@@ -15,33 +15,37 @@ import java.util.logging.Logger;
 import static junit.framework.TestCase.*;
 import static org.junit.Assert.assertNotEquals;
 
-public class DatabaseTest {
+public class DatabaseTest
+{
     private static final Logger LOGGER = Logger.getLogger(DatabaseTest.class.getName());
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bankPU");
 
     private EntityManager em;
 
     @Before
-    public void before() throws Exception {
+    public void before() throws Exception
+    {
         try
         {
             new DatabaseCleaner(emf.createEntityManager()).clean();
             em = emf.createEntityManager();
-        }
-        catch (Exception e)
+        } catch (Exception e)
         {
             LOGGER.log(Level.CONFIG, "Could not initiate connection to database", e);
         }
     }
 
     @After
-    public void after() throws Exception {
+    public void after() throws Exception
+    {
         em.close();
     }
 
     @Test
-    public void vraag1() throws Exception {
-        try {
+    public void vraag1() throws Exception
+    {
+        try
+        {
             Account account = new Account(111L);
             em.getTransaction().begin();
             em.persist(account);
@@ -54,15 +58,18 @@ public class DatabaseTest {
             // het id bestond al, de database werd nog niet gecleand, kijk naar de @before, hier staat de databasecleaner
             assertTrue(account.getId() > 0L);
 
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             LOGGER.log(Level.SEVERE, "Something went wrong during question 1", e);
             em.getTransaction().rollback();
         }
     }
 
     @Test
-    public void vraag2() throws Exception {
-        try {
+    public void vraag2() throws Exception
+    {
+        try
+        {
             Account account = new Account(111L);
             em.getTransaction().begin();
             em.persist(account);
@@ -71,15 +78,18 @@ public class DatabaseTest {
 // TODO code om te testen dat table account geen records bevat. Hint: bestudeer/gebruik AccountDAOJPAImpl
             assertEquals(0, new AccountDAOJPAImpl(em).count());
 
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             LOGGER.log(Level.SEVERE, "Something went wrong during question 2", e);
             em.getTransaction().rollback();
         }
     }
 
     @Test
-    public void vraag3() throws Exception {
-        try {
+    public void vraag3() throws Exception
+    {
+        try
+        {
             Long expected = -100L;
             Account account = new Account(111L);
             account.setId(expected);
@@ -93,11 +103,81 @@ public class DatabaseTest {
             em.getTransaction().commit();
             assertNotEquals(expected, account.getId());
 
-
-
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             LOGGER.log(Level.SEVERE, "Something went wrong during question 3", e);
             em.getTransaction().rollback();
         }
+    }
+
+    @Test
+    public void vraag4() throws Exception
+    {
+        Long expectedBalance = 400L;
+        Account account = new Account(114L);
+        em.getTransaction().begin();
+        em.persist(account);
+        account.setBalance(expectedBalance);
+        em.getTransaction().commit();
+        assertEquals(expectedBalance, account.getBalance()); // account balance wordt geset bij "account.setBalance(expectedBalance)"
+//TODO: verklaar de waarde van account.getBalance
+        Long cid = account.getId();
+        account = null;
+        EntityManager em2 = emf.createEntityManager();
+        em2.getTransaction().begin();
+        Account found = em2.find(Account.class, cid);
+//TODO: verklaar de waarde van found.getBalance
+        assertEquals(expectedBalance, found.getBalance()); // account balance wordt opgehaald om te zien of in een andere stream hetzelfde getal wordt gevonden en teruggestuurd
+
+        em2.close(); // added
+
+    }
+
+    @Test
+    public void vraag5() throws Exception
+    {
+        Account account = new Account(114L); //create account
+        account.setBalance(0L); //set initial balance
+
+        em.getTransaction().begin();
+        em.persist(account); //put the object in the database
+        em.getTransaction().commit();
+        Long cid = account.getId(); //hold the Id of the account to retrieve it later
+
+        EntityManager em2 = emf.createEntityManager(); //create a second connection
+        em2.getTransaction().begin();
+        Account found = em2.find(Account.class, cid); //get the same object from the database
+
+        em.getTransaction().begin();
+        account.setBalance(500L); //change the value from connection A
+        em.getTransaction().commit();
+
+        em2.refresh(found); //refresh connection 2
+
+        assertEquals(500L, found.getBalance().longValue()); //check if value changed in connection 2
+    }
+
+    @Test
+    public void vraag6() throws Exception
+    {
+
+    }
+
+    @Test
+    public void vraag7() throws Exception
+    {
+
+    }
+
+    @Test
+    public void vraag8() throws  Exception
+    {
+
+    }
+
+    @Test
+    public void vraag9() throws Exception
+    {
+
     }
 }
